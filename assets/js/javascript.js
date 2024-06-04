79b2027eeba03a0217dee212937ff98f2a54b875
willy0483 <willyjensen251@gmail.com>
2024-06-04 16:17:26 +0200

    yes

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,32 +1,41 @@
-import React, { useRef } from "react";
-import {
-  motion,
-  useMotionTemplate,
-  useMotionValue,
-  useSpring,
-} from "framer-motion";
-import { FiMousePointer } from "react-icons/fi";
+const pre = document.querySelector("pre");
+const cardsImg = document.getElementById("cardsImg");
 
-
-const handleMouseMove = document.getElementById("handleMouseMove");
-
-const x = useMotionValue(0);
-const y = useMotionValue(0);
-
-
-handleMouseMove.addEventListener("mousemove", (e) => {
-  const rect = e.target.getBoundingClientRect();
-
-
-
-  const width = rect.width;
-  const height = rect.height;
-
-  const mouseX = e.clientX - rect.left;
-  const mouseY = e.clientY - rect.top;
-
-  const xPct = mouseX / width - 0.5;
-  const yPct = mouseY / height - 0.5;
-
-  console.log(xPct);
+cardsImg.addEventListener("mousemove", (e) => {
+  rotateElement(e, pre);
 });
+
+cardsImg.addEventListener("mouseleave", () => {
+  resetElement(pre);
+});
+
+function rotateElement(e, element) {
+  // Remove transition for immediate effect on mousemove
+  element.style.transition = "none";
+
+  // get mouse position
+  const x = e.clientX;
+  const y = e.clientY;
+
+  // find the middle
+  const middleX = window.innerWidth / 2;
+  const middleY = window.innerHeight / 2;
+
+  // get offset from middle as a percentage
+  // and tone it down a little
+  const offsetX = ((x - middleX) / middleX) * 15;
+  const offsetY = ((y - middleY) / middleY) * 15;
+
+  // set rotation
+  element.style.setProperty("--rotateX", offsetX + "deg");
+  element.style.setProperty("--rotateY", -1 * offsetY + "deg");
+  element.style.transform = `perspective(5000px) rotateY(${offsetX}deg) rotateX(${-offsetY}deg)`;
+}
+
+function resetElement(element) {
+  // Add transition for smooth effect on mouse leave
+  element.style.transition = "transform 0.5s ease";
+  element.style.setProperty("--rotateX", "0deg");
+  element.style.setProperty("--rotateY", "0deg");
+  element.style.transform = "perspective(5000px) rotateY(0deg) rotateX(0deg)";
+}
